3487d227e2939a92eca9b7bb6b3a4b397ce34aa7
vijayanandc <vijayanandmku@gmail.com>
2025-10-28 11:58:10 +0530

    Update navbar with organization menu

---
+++ public/assets/js/config.js
@@ -0,0 +1,3 @@
+// Central configuration for AppBlocks API requests.
+// Update APPBLOCKS_BASE_URL to point to the desired server.
+window.APPBLOCKS_BASE_URL = window.APPBLOCKS_BASE_URL || "https://appblocks.in";
